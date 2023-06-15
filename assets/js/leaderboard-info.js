3581678d6f17471eab4b8d41e7dc294c701e2602
scene-replica <scenereplica.benchmark@gmail.com>
2023-06-15 17:12:34 -0500

    + leaderboard table from main manuscript

---
+++ assets/js/leaderboard-info.js
@@ -0,0 +1,19 @@
+// Model based grasping methods
+const leaderboard_model_based_grasping_entries = [
+    [1, 'PoseRBPF', 'GraspIt! + Top-Down', 'OMPL', 'MoveIt', 'Near-to-Far', '58/100', '64/100', '#'],
+    [1, 'PoseRBPF', 'GraspIt! + Top-Down', 'OMPL', 'MoveIt', 'Fixed', '59/100', '59/100', '#'],
+    [2, 'PoseCNN', 'GraspIt! + Top-Down', 'OMPL', 'MoveIt', 'Near-to-Far', '48/100', '50/100', '#'],
+    [2, 'PoseCNN', 'GraspIt! + Top-Down', 'OMPL', 'MoveIt', 'Fixed', '38/100', '44/100', '#'],
+]
+
+// Model free grasping methods here
+const leaderboard_model_free_grasping_entries = [
+    [3, 'UCN', 'GraspNet + Top-Down', 'OMPL', 'MoveIt', 'Near-to-Far', '43/100', '46/100', '#'],
+    [3, 'UCN', 'GraspNet + Top-Down', 'OMPL', 'MoveIt', 'Fixed', '39/100', '42/100', '#'],
+    [4, 'UCN', 'Contact-graspnet + Top-Down', 'OMPL', 'MoveIt', 'Near-to-Far', '60/100', '63/100', '#'],
+    [4, 'UCN', 'Contact-graspnet + Top-Down', 'OMPL', 'MoveIt', 'Fixed', '60/100', '64/100', '#'],
+    [5, 'MSMFormer', 'GraspNet + Top-Down', 'OMPL', 'MoveIt', 'Near-to-Far', '38/100', '41/100', '#'],
+    [5, 'MSMFormer', 'GraspNet + Top-Down', 'OMPL', 'MoveIt', 'Fixed', '36/100', '41/100', '#'],
+    [6, 'MSMFormer', 'Contact-graspnet + Top-Down', 'OMPL', 'MoveIt', 'Near-to-Far', '57/100', '65/100', '#'],
+    [6, 'MSMFormer', 'Contact-graspnet + Top-Down', 'OMPL', 'MoveIt', 'Fixed', '61/100', '70/100', '#'],
+]
