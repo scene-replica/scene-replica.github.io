b48451de218c6762f1ded3ddca354d48f9e4c249
scene-replica <scenereplica.benchmark@gmail.com>
2023-06-15 20:51:00 -0500

    + leaderboard icons; simply leaderboard js by maling one entry for each method

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,85 +1,92 @@
 function log(message) {
-    console.log(message);
+  console.log(message);
 }
 
 function get_leaderboard_entry(idx, entry) {
-    let th_content = ``
-    let n = entry.length
-    for (let [index, cell] of entry.entries()) {
-        if (index === n-1) {
-            th_content += '<td> <a style=""href="' + cell + '" target="_blank">Link</a></td>'
-        } else {
-            th_content += `<td>${cell}</td>`
-        }
-     }
-    // return `<tr><th>${idx+1}</th>${th_content}</tr>` Use for rank
-    return `<tr>${th_content}</tr>`
+  let n = entry.length
+  let row_prefix = `<td>${idx+1}</td>`
+  let th_n2f_content = ``
+  let th_frnd_content = ``
+  for (var i = 0; i < n; i += 1) {
+    if (i>=0 && i<4){
+      row_prefix += `<td>${entry[i]}</td>`;
+    }else if(i>=4 && i<8){
+      th_n2f_content += `<td>${entry[i]}</td>`;
+    }else if(i>=8 && i<n-1){
+      th_frnd_content += `<td>${entry[i]}</td>`;
+    }else{
+      var vid_link_html = `<td> <a style=""href="" target="_blank">Link</a></td>`
+      th_n2f_content += vid_link_html
+      th_frnd_content += vid_link_html
+    }
+  }
+
+  // return `<tr><th>${idx+1}</th>${th_content}</tr>` Use for rank
+  return `<tr>${row_prefix}${th_n2f_content}</tr><tr>${row_prefix}${th_frnd_content}</tr>`
 }
 
 $(document).ready(function () {
-    let leaderboard_body = `<tr>Model-Based Grasping</tr>`
-    for (let [index, leaderboard_entry] of leaderboard_model_based_grasping_entries.entries()) { 
-        leaderboard_body += get_leaderboard_entry(index, leaderboard_entry)
-    }
-    leaderboard_body += `<tr>Model-Free Grasping</tr>`
-    for (let [index, leaderboard_entry] of leaderboard_model_free_grasping_entries.entries()) { 
-        leaderboard_body += get_leaderboard_entry(index, leaderboard_entry)
-    }
-    $('#leaderboard-body').html(leaderboard_body);
+  let leaderboard_body = ''
 
-        // Get the table and table body elements
-        var table = $('#leaderboard-table');
-        var tbody = table.find('tbody');
-    
-        // Define the default sort column index
-        var defaultSortColumn = 6; // Set it to the desired column index (0-based)
-    
-        // Sort the default column by default
-        sortTable(defaultSortColumn);
-    
-        // Attach a click event handler to table headers
-        table.find('th').click(function() {
-          var column = $(this).index(); // Get the column index
-    
-          // Check if the column is already sorted
-          var isAscending = $(this).hasClass('sorted-asc');
-          table.find('th').removeClass('sorted-asc sorted-desc');
-    
-          // Sort the table rows based on the selected column and sort order
-          sortTable(column, !isAscending);
-    
-          // Update the sort order indicator
-          if (isAscending) {
-            $(this).removeClass('sorted-asc').addClass('sorted-desc');
-          } else {
-            $(this).removeClass('sorted-desc').addClass('sorted-asc');
-          }
-        });
-    
-        // Function to sort the table rows based on a given column index and sort order
-        function sortTable(column, isAscending) {
-          // Get the rows
-          var rows = tbody.find('tr').get();
-    
-          // Sort the rows based on the selected column and sort order
-          rows.sort(function(a, b) {
-            var aValue = $(a).find('td').eq(column).text().toUpperCase();
-            var bValue = $(b).find('td').eq(column).text().toUpperCase();
-            if (aValue < bValue) {
-              return isAscending ? -1 : 1;
-            }
-            if (aValue > bValue) {
-              return isAscending ? 1 : -1;
-            }
-            return 0;
-          });
-    
-          // Clear the table body and re-append the sorted rows
-          tbody.empty();
-          $.each(rows, function(index, row) {
-            tbody.append(row);
-          });
-        }
+  for (let [index, leaderboard_entry] of leaderboard_entries.entries()) {
+    leaderboard_body += get_leaderboard_entry(index, leaderboard_entry)
+  }
+
+  $('#leaderboard-body').html(leaderboard_body);
+
+  // Get the table and table body elements
+  var table = $('#leaderboard-table');
+  var tbody = table.find('tbody');
+
+  // Define the default sort column index
+  var defaultSortColumn = 7; // Set it to the desired column index (0-based)
+
+  // Sort the default column by default
+  sortTable(defaultSortColumn);
+
+  // Attach a click event handler to table headers
+  table.find('th').click(function () {
+    var column = $(this).index(); // Get the column index
+
+    // Check if the column is already sorted
+    var isAscending = $(this).hasClass('sorted-asc');
+    table.find('th').removeClass('sorted-asc sorted-desc');
+
+    // Sort the table rows based on the selected column and sort order
+    sortTable(column, !isAscending);
+
+    // Update the sort order indicator
+    if (isAscending) {
+      $(this).removeClass('sorted-asc').addClass('sorted-desc');
+    } else {
+      $(this).removeClass('sorted-desc').addClass('sorted-asc');
+    }
+  });
+
+  // Function to sort the table rows based on a given column index and sort order
+  function sortTable(column, isAscending) {
+    // Get the rows
+    var rows = tbody.find('tr').get();
+
+    // Sort the rows based on the selected column and sort order
+    rows.sort(function (a, b) {
+      var aValue = $(a).find('td').eq(column).text().toUpperCase();
+      var bValue = $(b).find('td').eq(column).text().toUpperCase();
+      if (aValue < bValue) {
+        return isAscending ? -1 : 1;
+      }
+      if (aValue > bValue) {
+        return isAscending ? 1 : -1;
+      }
+      return 0;
+    });
+
+    // Clear the table body and re-append the sorted rows
+    tbody.empty();
+    $.each(rows, function (index, row) {
+      tbody.append(row);
+    });
+  }
 })
 
 
--- assets/js/leaderboard-info.js
+++ assets/js/leaderboard-info.js
@@ -1,19 +1,10 @@
-// Model based grasping methods
-const leaderboard_model_based_grasping_entries = [
-    [1, 'PoseRBPF', 'GraspIt! + Top-Down', 'OMPL', 'MoveIt', 'Near-to-Far', '58/100', '64/100', '#'],
-    [1, 'PoseRBPF', 'GraspIt! + Top-Down', 'OMPL', 'MoveIt', 'Fixed', '59/100', '59/100', '#'],
-    [2, 'PoseCNN', 'GraspIt! + Top-Down', 'OMPL', 'MoveIt', 'Near-to-Far', '48/100', '50/100', '#'],
-    [2, 'PoseCNN', 'GraspIt! + Top-Down', 'OMPL', 'MoveIt', 'Fixed', '38/100', '44/100', '#'],
-]
-
-// Model free grasping methods here
-const leaderboard_model_free_grasping_entries = [
-    [3, 'UCN', 'GraspNet + Top-Down', 'OMPL', 'MoveIt', 'Near-to-Far', '43/100', '46/100', '#'],
-    [3, 'UCN', 'GraspNet + Top-Down', 'OMPL', 'MoveIt', 'Fixed', '39/100', '42/100', '#'],
-    [4, 'UCN', 'Contact-graspnet + Top-Down', 'OMPL', 'MoveIt', 'Near-to-Far', '60/100', '63/100', '#'],
-    [4, 'UCN', 'Contact-graspnet + Top-Down', 'OMPL', 'MoveIt', 'Fixed', '60/100', '64/100', '#'],
-    [5, 'MSMFormer', 'GraspNet + Top-Down', 'OMPL', 'MoveIt', 'Near-to-Far', '38/100', '41/100', '#'],
-    [5, 'MSMFormer', 'GraspNet + Top-Down', 'OMPL', 'MoveIt', 'Fixed', '36/100', '41/100', '#'],
-    [6, 'MSMFormer', 'Contact-graspnet + Top-Down', 'OMPL', 'MoveIt', 'Near-to-Far', '57/100', '65/100', '#'],
-    [6, 'MSMFormer', 'Contact-graspnet + Top-Down', 'OMPL', 'MoveIt', 'Fixed', '61/100', '70/100', '#'],
+const leaderboard_entries = [
+    // Model based grasping methods
+    ['PoseRBPF', 'GraspIt! + Top-Down', 'OMPL', 'MoveIt', 'Near-to-Far', 'Model-Based', '58/100', '64/100', 'Fixed Random', 'Model-Based', '59/100', '59/100', '#'],
+    ['PoseCNN', 'GraspIt! + Top-Down', 'OMPL', 'MoveIt', 'Near-to-Far', 'Model-Based', '48/100', '50/100', 'Fixed Random', 'Model-Based', '38/100', '44/100', '#'],
+    // Model free grasping methods here
+    ['UCN', 'GraspNet + Top-Down', 'OMPL', 'MoveIt', 'Near-to-Far', 'Model-Free', '43/100', '46/100', 'Fixed Random', 'Model-Free', '39/100', '42/100', '#'],
+    ['UCN', 'Contact-graspnet + Top-Down', 'OMPL', 'MoveIt', 'Near-to-Far', 'Model-Free', '60/100', '63/100', 'Fixed Random', 'Model-Free', '60/100', '64/100', '#'],
+    ['MSMFormer', 'GraspNet + Top-Down', 'OMPL', 'MoveIt', 'Near-to-Far', 'Model-Free', '38/100', '41/100', 'Fixed Random', 'Model-Free', '36/100', '41/100', '#'],
+    ['MSMFormer', 'Contact-graspnet + Top-Down', 'OMPL', 'MoveIt', 'Near-to-Far', 'Model-Free', '57/100', '65/100', 'Fixed Random', 'Model-Free', '61/100', '70/100', '#'],
 ]
