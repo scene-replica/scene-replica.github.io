a2402a2c7abff1d114b5b1105b3462ae12b4a395
scene-replica <scenereplica.benchmark@gmail.com>
2023-06-19 18:19:41 -0500

    + more leaderboard method:2 vids [not all vids] and message for vid in progress for empty video links

--- scene-videos/assets/js/index.js
+++ scene-videos/assets/js/index.js
@@ -13,22 +13,32 @@ $(document).ready(function () {
     $('#page-title').html(`Order: ${query['cat']}`)
 
     let vid_html = ``
-    let v_links = video_links[query['id']]
-    for (var i = 0; i < v_links.length; i++) {
-        if (v_links[i][query['vli']] != '#'){
+    let vid_in_making_html = `<div class="notification is-warning is-light center"><i class="fa fa-info-circle"></i> Video making in progress. Will be added soon. Thanks for your patience.</div>`
+    
+    try {
+        let v_links = video_links[query['id']]
+        let content = ``
+        for (var i = 0; i < v_links.length; i++) {
+            if (v_links[i][query['vli']] != '#'){
+                content = `<div class="publication-video">
+                <iframe width="560px" height="315px" src="${v_links[i][query['vli']]}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </div>`
+            }else{
+                content = vid_in_making_html
+            }
             vid_html += `
-            <div class="columns is-centered">
-            <div class="column">
-                    <div class="content has-text-centered">
-                        <div class="publication-video">
-                            <iframe width="560px" height="315px" src="${v_links[i][query['vli']]}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                            </div>
+                <div class="columns is-centered">
+                    <div class="column">
+                        <div class="content has-text-centered">
+                            ${content}
                             <p class="subtitle has-text-centered"><strong>Scene: ${v_links[i][0]}</strong></p>
+                        </div>
                     </div>
-                    </div>
-            </div>
+                </div>
             `
         }
-    }
-    $('#vid-items').html(vid_html)
+        $('#vid-items').html(vid_html)
+      } catch (error) {
+        $('#vid-items').html(vid_in_making_html)
+      }
 });
